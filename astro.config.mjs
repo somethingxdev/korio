7feed6033c5948890ae0f8f7b54a4236751f51ed
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2025-04-18 18:00:49 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS()],
+	experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Montserrat',
+        cssVariable: '--font-sans',
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
 });
---
+++ src/site.content.ts
@@ -0,0 +1,30 @@
+export const ADVANTAGES = [
+  {
+    card_1: [
+      {
+        title: 'Your All-in-One Styling Powerhouse!',
+        description: 'Why clutter your bathroom with seven different tools? The Vincofy 7-in-1 Air Styler replaces them all. Designed for speed, control and professional results, it delivers powerful airflow at 110,000 RPM to dry, straighten, curl, and volumise – fast and fuss-free.',
+        powerhouseItems: [
+          {
+            label: '7 Interchangeable Heads',
+            description: 'Includes curlers, straightening brush, volumiser, and more.',
+          },
+          {
+            label: 'Supercharged Drying',
+            description: 'Airflow up to 23m/s cuts drying time in half.',
+          },
+          {
+            label: 'Perfect for Any Hair Type',
+            description: '9 custom heat/speed settings to suit every texture and length.',
+          },
+        ],
+      },
+    ],
+    card_2: [
+      {
+        title: 'Customized Styling For Every Hair Type!',
+        description: 'No matter your hair texture – fine, thick, damaged, or dyed – Vincofy’s multi-mode system adapts. Just twist the dial to choose your ideal temperature and airflow combo.',
+      },
+    ],
+  },
+];
